f1b1609c1805649679a335c0336a99f98fada890
Dzhafar <djafarrn@gmail.com>
2019-10-09 15:46:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let matches = {};
+
+    bracketsConfig.forEach(brackets => matches[brackets[0]] = brackets[1]);
+    
+    for (let i = 0; i < str.length; i++) {
+      let bracket = str.charAt(i);
+      let isOpenBracket = matches[bracket];
+      let isOneSymbolBrackets = matches[bracket] == bracket;
+      let lastOpenBracket = stack[stack.length - 1];
+      const getLastOpenBracket = () => stack.pop();
+      const getMatchingClosingBracket = () => matches[getLastOpenBracket()];
+      
+      if (isOneSymbolBrackets && lastOpenBracket == bracket) {
+          stack.pop();
+      } else if (isOpenBracket) {
+          stack.push(bracket);
+      } else if (bracket != getMatchingClosingBracket()) {            
+          return false;
+      }
+    }    
+    return stack.length == 0;
 }
